5a78cb337aeab639ff5410fa15f9b4aef41e0fd7
Danz Moses <danzmoses@gmail.com>
2020-11-30 22:02:06 -0800

    Declared and implemented create_iterator for Op class

--- op.hpp
+++ op.hpp
@@ -8,6 +8,7 @@ class Op : public Base {
         Op(double value) : Base() { this->setValue(value); }
         virtual double evaluate() { return this->getValue(); }
         virtual std::string stringify() { return std::to_string(evaluate()); }
+        virtual Iterator* create_iterator() { return new NullIterator(this); }
 };
 
 #endif //__OP_HPP__
