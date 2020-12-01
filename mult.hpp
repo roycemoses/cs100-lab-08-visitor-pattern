799d98626d6d326656d6728cfa2c93f31aef6741
Danz Moses <danzmoses@gmail.com>
2020-11-30 22:01:57 -0800

    Declared and implemented create_iterator for Mult class

--- mult.hpp
+++ mult.hpp
@@ -11,6 +11,7 @@ class Mult : public Base {
         Mult(Base* value1, Base* value2) : value1(value1), value2(value2), Base() { this->setValue(evaluate()); }
         virtual double evaluate() { return value1->getValue() * value2->getValue(); }
         virtual std::string stringify() { return value1->stringify() + std::string(" * ") + value2->stringify(); }
+        virtual Iterator* create_iterator() { return new BinaryIterator(this); }
 };
 
 #endif // __MULT_HPP__
