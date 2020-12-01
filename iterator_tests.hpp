586a281ce6c6af6472cd2c8281927b8e44c9c92d
roycemoses <royce.kmoses@gmail.com>
2020-11-30 23:41:20 -0800

    Added more checks for SubTests and also fixed IterateLargerSub

--- iterator_tests.hpp
+++ iterator_tests.hpp
@@ -18,13 +18,55 @@ TEST(CreateIteratorTests, IterateSub)
 
     Iterator* it = sub->create_iterator();
 
+    EXPECT_FALSE(it->is_done());
+
     EXPECT_EQ(it->current()->evaluate(), 2);
     EXPECT_EQ(it->current()->stringify(), "2.000000");
 
     it->next();
 
+    EXPECT_FALSE(it->is_done());
+
     EXPECT_EQ(it->current()->evaluate(), 1);
     EXPECT_EQ(it->current()->stringify(), "1.000000");
+
+    it->next();
+
+    EXPECT_TRUE(it->is_done());
+    EXPECT_TRUE(it->current() == nullptr);
+}
+
+TEST(CreateIteratorTests, IterateLargerSub)
+{
+    Base* value1 = new Op(2);
+    Base* value2 = new Op(1);
+    Base* sub = new Sub(value1, value2);
+    Base* value3 = new Op(10);
+    Base* value4 = new Op(9);
+    Base* sub2 = new Sub(value3, value4);
+    Base* sub3 = new Sub(sub, sub2);
+
+    EXPECT_EQ(sub3->evaluate(), 0);
+    EXPECT_EQ(sub3->stringify(), "2.000000 - 1.000000 - 10.000000 - 9.000000");
+
+    Iterator* it = sub3->create_iterator();
+
+    EXPECT_FALSE(it->is_done());
+
+    EXPECT_EQ(it->current()->evaluate(), 1);
+    EXPECT_EQ(it->current()->stringify(), "2.000000 - 1.000000");
+
+    it->next();
+
+    EXPECT_FALSE(it->is_done());
+
+    EXPECT_EQ(it->current()->evaluate(), 1);
+    EXPECT_EQ(it->current()->stringify(), "10.000000 - 9.000000");
+
+    it->next();
+
+    EXPECT_TRUE(it->is_done());
+    EXPECT_TRUE(it->current() == nullptr);
 }
 
 TEST(CreateIteratorTests, IterateDiv)
