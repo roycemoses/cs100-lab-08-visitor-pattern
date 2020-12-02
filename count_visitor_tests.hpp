6a8562eae406d1c5cb45ee9bc546944ed43d1287
roycemoses <royce.kmoses@gmail.com>
2020-12-02 15:04:31 -0800

    Added CountSubs test

---
+++ count_visitor_tests.hpp
@@ -0,0 +1,59 @@
+#ifndef COUNT_VISITOR_TESTS_HPP
+#define COUNT_VISITOR_TESTS_HPP
+
+#include "gtest/gtest.h"
+#include "op.hpp"
+#include "add.hpp"
+#include "mult.hpp"
+#include "pow.hpp"
+#include "sub.hpp"
+#include "div.hpp"
+#include "rand.hpp"
+
+TEST(CountVisitorTests, CountSub)
+{
+    CountVisitor cv;
+    Base* value1 = new Op(2);
+    Base* value2 = new Op(1);
+    Base* sub = new Sub(value1, value2);
+    Base* rand = new Rand();
+
+    Base* dummy = new Sub(sub, rand);
+    Iterator* it = new PreorderIterator(dummy);
+    it->first();
+
+    do {
+        it->current()->accept(&cv);
+        it->next();
+    } while (!it->is_done());
+
+    EXPECT_EQ(cv.sub_count(), 1);
+    EXPECT_EQ(cv.op_count(), 2);
+    EXPECT_EQ(cv.rand_count(), 1);
+}
+
+TEST(CountVisitorTests, CountSubs)
+{
+    CountVisitor cv;
+    Base* value1 = new Op(2);
+    Base* value2 = new Op(1);
+    Base* sub = new Sub(value1, value2); // 2 - 1
+    Base* sub2 = new Sub(value2, sub); // 1 - (2 - 1)
+    Base* sub3 = new Sub(value1, sub2); // 2 - (1 - (2 - 1))
+    Base* rand = new Rand();
+
+    Base* dummy = new Sub(sub3, rand);
+    Iterator* it = new PreorderIterator(dummy);
+    it->first();
+
+    do {
+        it->current()->accept(&cv);
+        it->next();
+    } while (!it->is_done());
+
+    EXPECT_EQ(cv.sub_count(), 3);
+    EXPECT_EQ(cv.op_count(), 4);
+    EXPECT_EQ(cv.rand_count(), 1);
+}
+
+#endif // COUNT_VISITOR_TESTS_HPP
