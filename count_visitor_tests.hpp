86a05a9721d38712656178b9df43811678b764a7
roycemoses <royce.kmoses@gmail.com>
2020-12-02 15:06:20 -0800

    Added CountNoDivs test

--- count_visitor_tests.hpp
+++ count_visitor_tests.hpp
@@ -75,4 +75,23 @@ TEST(CountVisitorTests, CountNoSubs)
     EXPECT_EQ(cv.rand_count(), 2);
 }
 
+TEST(CountVisitorTests, CountNoDivs)
+{
+    CountVisitor cv;
+    Base* value1 = new Rand();
+    Base* rand = new Rand();
+
+    Base* dummy = new Sub(value1, rand);
+    Iterator* it = new PreorderIterator(dummy);
+    it->first();
+
+    do {
+        it->current()->accept(&cv);
+        it->next();
+    } while (!it->is_done());
+
+    EXPECT_EQ(cv.div_count(), 0);
+    EXPECT_EQ(cv.rand_count(), 2);
+}
+
 #endif // COUNT_VISITOR_TESTS_HPP
