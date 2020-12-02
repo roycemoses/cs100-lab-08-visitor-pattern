380f0d9a0d67cd300eb2fc14a2680c5db1871042
Danz Moses <danzmoses@gmail.com>
2020-12-01 21:09:21 -0800

    Added CountAdd test for CountVisitorTests

---
+++ count_visitor_tests.hpp
@@ -0,0 +1,33 @@
+#ifndef COUNT_VISITOR_TESTS_HPP
+#define COUNT_VISITOR_TESTS_HPP
+
+#include "gtest/gtest.h"
+#include "op.hpp"
+#include "add.hpp"
+#include "mult.hpp"
+#include "pow.hpp"
+#include "sub.hpp"
+#include "div.hpp"
+#include "rand.hpp"
+
+TEST(CountVisitorTests, CountAdd)
+{
+    CountVisitor cv;
+    Base* rand = new Rand;
+    Base* two = new Op(2);
+    Base* four = new Op(4);
+    Base* add = new Add(two, four);
+    Base* dummy = new Add(add, rand);
+    Iterator* it = new PreorderIterator(dummy);
+    it->first();
+    do {
+        it->current()->accept(&cv);
+        it->next();
+    } while (!it->is_done());
+
+    EXPECT_EQ(cv.add_count(), 1);
+    EXPECT_EQ(cv.op_count(), 2);
+    EXPECT_EQ(cv.rand_count(), 1);
+}
+
+#endif // COUNT_VISITOR_TESTS_HPP
